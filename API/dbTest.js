83bfb4c75bf7e3569a4e1e202d02bc29a25dcd19
andreibuntsev <andrew.buntsev@gmail.com>
2020-07-17 11:34:46 +1000

    deleted port

--- API/dbTest.js
+++ API/dbTest.js
@@ -8,8 +8,7 @@ const pool = mysql.createPool(
         host: process.env.RDS_HOSTNAME,
         user: process.env.RDS_USERNAME,
         password: process.env.RDS_PASSWORD,
-        database: 'til',
-        port: process.env.RDS_PORT
+        database: 'til'
     }
 );
 
